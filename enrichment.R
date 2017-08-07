1f7450e25ce4c9321a4abef5c89f324e9e8f3399
olegkagan <jodyye88@gmail.com>
2017-08-07 09:54:01 +0100

    Update enrichment.R

--- enrichment.R
+++ enrichment.R
@@ -1,7 +1,66 @@
 # mQTL enrichment analysis for Data1
 
-#load GWAS summary statistics (Data1)
-
+#########################################################
+#load GWAS summary statistics (Data1) and merge LD score
+#########################################################
 
 library(data.table)
 gwasdata<-fread("meta_all.txt",fill=TRUE)
+
+#reformat Data1
+
+setnames(gwasdata,old=c("chromosome","position","a0", "a1", "EUR_MAF_1kG",
+                        "rsid","majmin","aff.pvalue","aff.OR","aff.se","ill.pvalue",
+                        "ill.OR","ill.se","OR.meta","beta.meta","se.meta","z.meta",
+                        "p.meta","max_posterior.A","info_score.A","cases_maf.A","controls_maf.A",
+                        "max_posterior.I","info_score.I","cases_maf.I","controls_maf.I","qc.check",
+                        "warnings","V29"),new=c("chromosome","chr", "positition","a0", "a1", 
+                                                "EUR_MAF_1kG","rsid","majmin","aff.pvalue","aff.OR",
+                                                "aff.se","ill.pvalue","ill.OR","ill.se","OR.meta","beta.meta",
+                                                "se.meta","z.meta","p.meta","max_posterior.A","info_score.A",
+                                                "cases_maf.A","controls_maf.A","max_posterior.I","info_score.I",
+                                                "cases_maf.I","controls_maf.I","qc.check","warnings"))
+
+
+#extraction LD score for CEU panel and save in local drive
+#LD score can be downloaded from https://github.com/bulik/ldsc
+
+
+chr1<-read.table(gzfile("1.l2.ldscore.gz"),header=T)
+chr2<-read.table(gzfile("2.l2.ldscore.gz"),header=T)
+chr3<-read.table(gzfile("3.l2.ldscore.gz"),header=T)
+chr4<-read.table(gzfile("4.l2.ldscore.gz"),header=T)
+chr5<-read.table(gzfile("5.l2.ldscore.gz"),header=T)
+chr6<-read.table(gzfile("6.l2.ldscore.gz"),header=T)
+chr7<-read.table(gzfile("7.l2.ldscore.gz"),header=T)
+chr8<-read.table(gzfile("8.l2.ldscore.gz"),header=T)
+chr9<-read.table(gzfile("9.l2.ldscore.gz"),header=T)
+chr10<-read.table(gzfile("10.l2.ldscore.gz"),header=T)
+chr11<-read.table(gzfile("11.l2.ldscore.gz"),header=T)
+chr12<-read.table(gzfile("12.l2.ldscore.gz"),header=T)
+chr13<-read.table(gzfile("13.l2.ldscore.gz"),header=T)
+chr14<-read.table(gzfile("14.l2.ldscore.gz"),header=T)
+chr15<-read.table(gzfile("15.l2.ldscore.gz"),header=T)
+chr16<-read.table(gzfile("16.l2.ldscore.gz"),header=T)
+chr17<-read.table(gzfile("17.l2.ldscore.gz"),header=T)
+chr18<-read.table(gzfile("18.l2.ldscore.gz"),header=T)
+chr19<-read.table(gzfile("19.l2.ldscore.gz"),header=T)
+chr20<-read.table(gzfile("20.l2.ldscore.gz"),header=T)
+chr21<-read.table(gzfile("21.l2.ldscore.gz"),header=T)
+chr22<-read.table(gzfile("22.l2.ldscore.gz"),header=T)
+
+merge<-rbind(chr1,chr2,chr3,chr4,chr5,chr6,chr7,chr8,chr9,chr10,chr11,chr12,chr13,chr14,chr15,chr16,chr17,chr18,chr19,chr20,chr21,chr22)
+
+
+#merge the ldscore with GWAS summary statistics Data1
+
+Data1_hg19<-merge(x=merge,y=gwasdata,by.x='SNP',by.y='rsid')
+#after merging, 1,167,927 SNPs are avaialble
+
+Data1_hg19_ldscore<-subset(Data1_hg19,select=c("SNP","CHR","BP","CM","MAF","L2","p.meta"))
+
+##############################
+#retrieve mQTL from adolescent
+##############################
+
+
