9ee26535a44b112cec581f023ee302604167230f
olegkagan <jodyye88@gmail.com>
2017-08-07 09:58:37 +0100

    Update enrichment.R

--- enrichment.R
+++ enrichment.R
@@ -1,13 +1,13 @@
 # mQTL enrichment analysis for Data1
 
 #########################################################
-#load GWAS summary statistics (Data1) and merge LD score
+# load GWAS summary statistics (Data1) and merge LD score
 #########################################################
 
 library(data.table)
 gwasdata<-fread("meta_all.txt",fill=TRUE)
 
-#reformat Data1
+# reformat Data1
 
 setnames(gwasdata,old=c("chromosome","position","a0", "a1", "EUR_MAF_1kG",
                         "rsid","majmin","aff.pvalue","aff.OR","aff.se","ill.pvalue",
@@ -22,8 +22,8 @@ setnames(gwasdata,old=c("chromosome","position","a0", "a1", "EUR_MAF_1kG",
                                                 "cases_maf.I","controls_maf.I","qc.check","warnings"))
 
 
-#extraction LD score for CEU panel and save in local drive
-#LD score can be downloaded from https://github.com/bulik/ldsc
+# extraction LD score for CEU panel and save in local drive
+# LD score can be downloaded from https://github.com/bulik/ldsc
 
 
 chr1<-read.table(gzfile("1.l2.ldscore.gz"),header=T)
@@ -52,15 +52,45 @@ chr22<-read.table(gzfile("22.l2.ldscore.gz"),header=T)
 merge<-rbind(chr1,chr2,chr3,chr4,chr5,chr6,chr7,chr8,chr9,chr10,chr11,chr12,chr13,chr14,chr15,chr16,chr17,chr18,chr19,chr20,chr21,chr22)
 
 
-#merge the ldscore with GWAS summary statistics Data1
+# merge the ldscore with GWAS summary statistics Data1
 
 Data1_hg19<-merge(x=merge,y=gwasdata,by.x='SNP',by.y='rsid')
-#after merging, 1,167,927 SNPs are avaialble
+# after merging, 1,167,927 SNPs are avaialble
 
 Data1_hg19_ldscore<-subset(Data1_hg19,select=c("SNP","CHR","BP","CM","MAF","L2","p.meta"))
 
-##############################
-#retrieve mQTL from adolescent
-##############################
+##############################################################################
+# retrieve mQTL from adolescent and perform Fisher's combined probability test
+##############################################################################
+
+library(TwoSampleMR)
+library(MRInstruments)
+data("aries_mqtl")
+cpglist<-subset(aries_mqtl,age=='Adolescence')
+# remove duplicated mQTLs from cpglist
+cpglist<-subset(cpglist,!duplicated(SNP))
+
+# merge Aries mQTLs with Data1
+available_mQTL<-merge(x=Data1_hg19_ldscore,y=cpglist,by.x='SNP',by.y='SNP')
+# obtained 6062 mQTLs
+
+# select mQTLs that have strong exposure effect 
+available_mQTL_test<-subset(available_mQTL,pval<1e-14)
+str(available_mQTL_test)
+
+# this gives n=4562 mQTLs to test
+# perform fisher's combined test
+combined.p.for.mQTL<-fishers_combined_test(available_mQTL_test$p.meta)
+# this gives pval = 2.312334e-42
+
+
+#######################################
+#matching null SNPs via SNP properties
+#######################################
+
+# null SNPs must be further than 1000kb away from the mQTL
+# max MAF devidation from reference SNP is 0.02
+# LDscore L2 need to be in the same bin
+
 
 
